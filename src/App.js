c2b4d4695b5687262a50b3e56ef6ab748cfd73a8
Shiva Salehnia <shiva.salehnia@gmail.com>
2023-08-26 00:34:05 -0700

    Adding open source link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Weather App</header>
+      <footer>
+        This project is{" "}
+        <a
+          href="https://github.com/ShivaSalehnia/weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open source
+        </a>{" "}
+      </footer>
     </div>
   );
 }
