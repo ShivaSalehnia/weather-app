58a35b209f7ed3d23db899d5f38dca08fd577c78
Shiva Salehnia <shiva.salehnia@gmail.com>
2023-08-26 01:22:12 -0700

    Adding Bootsrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather App</header>
-      <footer>
-        This project is{" "}
-        <a
-          href="https://github.com/ShivaSalehnia/weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open source
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <header className="App-header">Weather App</header>
+        <footer>
+          This project is{" "}
+          <a
+            href="https://github.com/ShivaSalehnia/weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>{" "}
+          by Shiva Salehnia
+        </footer>
+      </div>
     </div>
   );
 }
